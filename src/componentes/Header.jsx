b2b0afbdb993edba3d7ebc0425b17b3f41339b42
marisergio <marisergio@gmail.com>
2025-03-07 01:26:25 -0300

    validacao form

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,30 +1,41 @@
 import style from './Header.module.css';
 
-export function Header({setFiltro}) {
+export function Header({ setFiltro }) {
 
-    function filtrar(event){
+    function filtrar(event) {
         event.preventDefault();
         const filtro = event.target.filtrar.value;
         console.log(filtro);
         event.target.filtrar.value = '';
+
     }
 
     return (
         <header className={style.header}>
             <h1 className={style.appName}>LITERA</h1>
             <div className={style.headerRight}>
-            <div className={style.searchBar}>
-                <form onSubmit={filtrar}>
-                    <input name="filtrar" type="text" placeholder="Pesquisar..." onChange={(e)=>setFiltro(e.target.value)} />
-                </form>
-            </div>
-            <div className={style.profileMenu}>
-                <button className={style.profileButton}>PERFIL</button>
-                <div className={style.dropdown}>
-                <a href="#">Alterar Senha</a>
-                <a href="#">Sair</a>
-            </div>
-            </div>
+                <div className={style.searchBar}>
+                    <form onSubmit={filtrar}>
+                        <input
+                            name="filtrar"
+                            type="text"
+                            placeholder="Pesquisar..."
+                            onChange={(e) => {
+                                event.target.setCustomValidity('');
+                                setFiltro(e.target.value)
+                            }}
+                            onInvalid={(e) => e.target.setCustomValidity('Digite algo para pesquisar')}
+                            required
+                        />
+                    </form>
+                </div>
+                <div className={style.profileMenu}>
+                    <button className={style.profileButton}>PERFIL</button>
+                    <div className={style.dropdown}>
+                        <a href="#">Alterar Senha</a>
+                        <a href="#">Sair</a>
+                    </div>
+                </div>
             </div>
 
         </header>
